d1465e74af52dbe65491c5573cca12142d6aec88
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-09-24 00:28:15 +0300

    Update index.js

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/auth');  // Use new auth routes
 const coursesRoute = require('./routes/courses'); // Your previous routes
 const userRoutes = require('./routes/userRoutes');
 const checkRole = require('./routes/checkRole');
-const Model = require('./models/User'); // Adjust the path as needed
+const model = require('./models/User'); // Adjust the path as needed
 
 const app = express();
 
